9aebba46c01edbe393f0ba6bd7986fd621ad5d0c
Atanu-Sarkar <atanu.noida@gmail.com>
2016-04-14 23:30:13 +0530

    Sort an array and insert an element inside it
    Java Session4 Assignment2

---
+++ SortArray.java
@@ -0,0 +1,15 @@
+package com.acadgild.session.four;
+
+import java.util.Arrays;
+
+public class SortArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {2,1,8,3,6};
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
+
